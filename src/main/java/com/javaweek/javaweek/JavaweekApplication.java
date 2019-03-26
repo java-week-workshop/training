b65b4fce84fba8b56f85152b9c3e869134c1a150
Rajneesh <er.rajyd@gmail.com>
2019-03-26 11:33:43 +0530

    Initial java week

---
+++ src/main/java/com/javaweek/javaweek/JavaweekApplication.java
@@ -0,0 +1,13 @@
+package com.javaweek.javaweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaweekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaweekApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/javaweek/javaweek/exception/Employee.java
@@ -0,0 +1,71 @@
+package com.javaweek.javaweek.exception;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+
+    private String code;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String age;
+
+    public Employee() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public static void main(String args[]){
+        System.out.println("Hello java week");
+    }
+
+
+}
